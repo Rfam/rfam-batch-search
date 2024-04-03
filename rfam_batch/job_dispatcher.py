716122d3b75c177a0ed688a0529480956520701a
carlosribas <caduribas@gmail.com>
2024-04-03 12:01:24 +0100

    Add function to fetch data from sequence and tblout enpoints

--- rfam_batch/job_dispatcher.py
+++ rfam_batch/job_dispatcher.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
 import typing as ty
-import datetime as dt
 
 import aiohttp
 from fastapi import HTTPException
-from pydantic import BaseModel
-
-from rfam_batch import api
 
 INFERNAL_CMSCAN_BASE_URL = "https://www.ebi.ac.uk/Tools/services/rest/infernal_cmscan"
 
@@ -30,35 +26,6 @@ class Query:
 
 class JobSubmitResult:
     job_id: str
-
-
-# class FinishedJob:
-#     async def infernal_results(self) -> ty.Optional[api.CmScanResult]:
-#         try:
-#             # Use the JobDispatcher to fetch the status of the job
-#             job_status = await JobDispatcher().status(self.job_id)
-
-#             # Check if the job is finished (you can customize this condition)
-#             if job_status.status == "done":
-#                 # Use the JobDispatcher to fetch the results
-#                 results = await JobDispatcher().fetch_results(self.job_id)
-
-#                 # If results are available, create a CmScanResult instance and return it
-#                 return api.CmScanResult.build(
-#                     closed=results.closed,
-#                     search_sequence=results.search_sequence,
-#                     hits=results.hits,
-#                     opened=results.opened,
-#                     num_hits=results.num_hits,
-#                     started=results.started,
-#                     job_id=results.job_id,
-#                 )
-#         except Exception as e:
-#             # Handle any exceptions or errors during the process
-#             print(f"Failed to retrieve infernal results: {str(e)}")
-
-#         # If results are not available or an error occurs, return None
-#         return None
 
 
 class JobDispatcher:
@@ -89,7 +56,20 @@ class JobDispatcher:
                 status_code=500, detail=f"An unexpected error occurred: {e}"
             )
 
-    async def cmscan_result(self, job_id: str):
+    async def cmscan_result(self, job_id: str) -> str:
         async with self.client.get(f"{INFERNAL_CMSCAN_BASE_URL}/result/{job_id}/out") as r:
-            response_text = await r.text()
-            return response_text
+            if r.status == 200:
+                response_text = await r.text()
+            return response_text if response_text else ""
+
+    async def cmscan_sequence(self, job_id: str) -> str:
+        async with self.client.get(f"{INFERNAL_CMSCAN_BASE_URL}/result/{job_id}/sequence") as r:
+            if r.status == 200:
+                sequence = await r.text()
+            return sequence if sequence else ""
+
+    async def cmscan_tblout(self, job_id: str) -> str:
+        async with self.client.get(f"{INFERNAL_CMSCAN_BASE_URL}/result/{job_id}/tblout") as r:
+            if r.status == 200:
+                tblout_text = await r.text()
+            return tblout_text if tblout_text else ""
